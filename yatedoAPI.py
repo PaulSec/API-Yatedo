9118ec2f5282a91dd0bc8032fbf741955c69dbc0
Paul AMAR <aos.paul@gmail.com>
2014-09-29 21:16:16 +0100

    Added verbose mode

--- yatedoAPI.py
+++ yatedoAPI.py
@@ -6,11 +6,6 @@ Using this code, you can manage to retrieve employees from a specific company
 import requests
 from bs4 import BeautifulSoup
 import re
-import json
-
-
-def display_message(s):
-    print '[verbose] %s' % s
 
 
 class YatedoAPI(object):
@@ -20,6 +15,10 @@ class YatedoAPI(object):
     """
 
     _instance = None
+    _verbose = False
+
+    def __init__(self, arg=None):
+        pass
 
     def __new__(cls, *args, **kwargs):
         """
@@ -28,11 +27,17 @@ class YatedoAPI(object):
         if not cls._instance:
             cls._instance = super(YatedoAPI, cls).__new__(
                 cls, *args, **kwargs)
+            if (args and args[0] and args[0]['verbose']):
+                cls._verbose = True
         return cls._instance
+
+    def display_message(self, s):
+        if (self._verbose):
+            print '[verbose] %s' % s
 
     def get_number_of_results(self, company_name):
         url = 'http://www.yatedo.com/s/companyname:(%s)/normal' % (company_name)
-        display_message(url)
+        self.display_message(url)
         req = requests.get(url)
         if 'did not match any' in req.content:
             return 0
@@ -40,7 +45,7 @@ class YatedoAPI(object):
 
     def search(self, company_name, start_index, page):
         url = 'http://www.yatedo.com/search/profil?c=normal&q=companyname:(%s)&rlg=en&uid=-1&start=%s&p=%s' % (company_name, start_index, page)
-        display_message(url)
+        self.display_message(url)
         req = requests.get(url)
         soup = BeautifulSoup(req.content)
         res = []
@@ -50,7 +55,7 @@ class YatedoAPI(object):
             contact_name = contact_name.text
             contact_job = contact_job.text[:-1]
 
-            print "%s (%s)" % (contact_name, contact_job)
+            self.display_message("%s (%s)" % (contact_name, contact_job))
             # creating structure for the contact
             contact = {}
             contact['name'] = contact_name
@@ -59,17 +64,17 @@ class YatedoAPI(object):
         return res
 
     def get_employees(self, company_name):
-        print 'Fetching result for company "%s"' % (company_name)
+        self.display_message('Fetching result for company "%s"' % (company_name))
         num = int(self.get_number_of_results(company_name))
 
         if num == 0:
-            print 'Stopping here, no results for %s' % company_name
+            self.display_message('Stopping here, no results for %s' % company_name)
             return []
 
         res = {}
         res['company_name'] = company_name
         res['employees'] = []
-        print 'Found %s results, collecting them..' % (num)
+        self.display_message('Found %s results, collecting them..' % (num))
         i = 0
         while i * 16 < num:
             new_employees = self.search(company_name, i * 16, i + 1)
