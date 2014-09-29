97085d0f7700a9af8e8f213a4f8866c72a863ec8
Paul AMAR <aos.paul@gmail.com>
2014-09-29 21:16:53 +0100

    Updated the example for using the API

--- yatedoAPI_example.py
+++ yatedoAPI_example.py
@@ -1,3 +1,9 @@
 from yatedoAPI import YatedoAPI
+
+# verbose mode
+res = YatedoAPI({'verbose': True}).get_employees('CompanyName')
+print res  # retrieves the results
+
+# non verbose mode
 res = YatedoAPI().get_employees('CompanyName')
 print res  # retrieves the results
